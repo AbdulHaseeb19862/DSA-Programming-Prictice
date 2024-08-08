28eaebfc6b0be1cba69316a644025308daca4f93
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-08-08 06:25:15 +0500

    factorial and summation of numbers

---
+++ Factorial/index.js
@@ -0,0 +1,15 @@
+//Find The Factorial
+// var n = 5;
+// var fact = 1;
+// for (var i = 1; i <= n; i++) {
+//   fact = fact * i;
+// }
+// console.log(`Factorial is = ${fact}`);
+
+// Find The Sum of 1 to 5;
+var n = 5;
+var sum = 0;
+for (var num = 1; num <= 5; num++) {
+  sum = sum + num;
+}
+console.log(`The Sum Of 1 to 5 is = ${sum}`);
